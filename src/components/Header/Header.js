0cdff9579dfa6002cf6a4498d41dc8efe12cbee7
binh <khanhbinh1186@gmail.com>
2022-12-22 21:47:33 +0700

    index route

---
+++ src/components/Header/Header.js
@@ -0,0 +1,33 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { NavLink } from 'react-router-dom'
+import './Header.scss'
+function Header() {
+    return (
+        <Navbar bg="light" expand="lg" className="nav-container">
+            <Container>
+                <NavLink className="navbar-brand" to="/">KB</NavLink>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <NavLink className="nav-link" to="/">Home</NavLink>
+                        <NavLink className="nav-link" to="user">User</NavLink>
+                        <NavLink className="nav-link" to="admin">Admin</NavLink>
+                    </Nav>
+                    <Nav>
+                        <NavDropdown title="Setting" id="basic-nav-dropdown">
+                            <NavDropdown.Item >Profile</NavDropdown.Item>
+                            <NavDropdown.Item > Log in</NavDropdown.Item>
+                            <NavDropdown.Item >Log out</NavDropdown.Item>
+
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,23 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import User from './components/User/User'
+import Admin from './components/Admin/Admin'
+import HomePage from './components/Home/HomePage';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+          <Route path="user" element={<User />} />
+          <Route path="admin" element={<Admin />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </Provider>
 );
 
